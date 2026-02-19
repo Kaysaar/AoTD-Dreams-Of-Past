e1294e2cd6ebc6164b6b799fc687ee3005e1b276
NebulaModLab <NebulaModding@hotmail.com>
2026-02-19 21:39:09 +0100

    Fixed vanilla cryo-revival facility still showing
    Made a new class to replace the vanilla cryo-revival facility's plugin with to keep it from showing up

---
+++ src/data/scripts/industry/DisabledVanillaCryoRevIndustry.java
@@ -0,0 +1,152 @@
+package data.scripts.industry;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.SectorEntityToken;
+import com.fs.starfarer.api.campaign.comm.CommMessageAPI;
+import com.fs.starfarer.api.campaign.econ.Industry;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.impl.campaign.econ.impl.BaseIndustry;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
+import com.fs.starfarer.api.impl.campaign.intel.BaseIntelPlugin;
+import com.fs.starfarer.api.impl.campaign.intel.MessageIntel;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
+import data.plugins.AodCryosleeperPLugin;
+
+public class DisabledVanillaCryoRevIndustry extends BaseIndustry {
+    @Override
+    public boolean showShutDown() {
+        return !isReawakening;
+    }
+
+
+    public SectorEntityToken tiedEntity = null;
+
+    public float interval = 0f;
+    public boolean isReawakening = false;
+    public boolean metDemand = true;
+
+    public void setReawakening(boolean reawakening) {
+        this.isReawakening = reawakening;
+    }
+
+    @Override
+    public boolean canInstallAICores() {
+        return false;
+    }
+
+    @Override
+    public boolean canImprove() {
+        return false;
+    }
+
+    @Override
+    public void apply() {
+        super.apply(true);
+        demand(Commodities.ORGANICS, 8);
+        if (getMaxDeficit(Commodities.ORGANICS).two != 0) {
+            metDemand = true;
+        } else {
+            metDemand = false;
+        }
+    }
+
+    @Override
+    public void unapply() {
+        super.unapply();
+    }
+
+    @Override
+    public void finishBuildingOrUpgrading() {
+        building = false;
+        buildProgress = 0;
+        buildTime = 1f;
+
+        buildingFinished();
+        reapply();
+
+    }
+
+    @Override
+    protected void sendBuildOrUpgradeMessage() {
+
+        if (market.isPlayerOwned()) {
+            if (isReawakening) {
+                isReawakening = false;
+                int size = 7;
+                if (tiedEntity.getCustomEntityType().equals("ark")) {
+                    size= (int) AodCryosleeperPLugin.sizeArk;
+                }
+                if (tiedEntity.getCustomEntityType().equals("derelict_cryosleeper")) {
+                    size = (int) AodCryosleeperPLugin.sizeCryosleeper;
+                }
+                grow(size);
+                MessageIntel intel = new MessageIntel(getCurrentName() + " at " + market.getName(), Misc.getBasePlayerColor());
+                intel.addLine(BaseIntelPlugin.BULLET + "Re-awakening procedure finished!");
+                intel.setIcon(Global.getSector().getPlayerFaction().getCrest());
+                intel.setSound(BaseIntelPlugin.getSoundStandardUpdate());
+                Global.getSector().getCampaignUI().addMessage(intel, CommMessageAPI.MessageClickAction.COLONY_INFO, market);
+            } else {
+                MessageIntel intel = new MessageIntel(getCurrentName() + " at " + market.getName(), Misc.getBasePlayerColor());
+                intel.addLine(BaseIntelPlugin.BULLET + "Construction completed");
+                intel.setIcon(Global.getSector().getPlayerFaction().getCrest());
+                intel.setSound(BaseIntelPlugin.getSoundStandardUpdate());
+                Global.getSector().getCampaignUI().addMessage(intel, CommMessageAPI.MessageClickAction.COLONY_INFO, market);
+            }
+
+        }
+    }
+
+    @Override
+    protected void addPostDemandSection(TooltipMakerAPI tooltip, boolean hasDemand, Industry.IndustryTooltipMode mode) {
+        super.addPostDemandSection(tooltip, hasDemand, mode);
+        if (isReawakening) {
+            tooltip.addPara("Re-awakening procedure has started", Misc.getPositiveHighlightColor(), 10f);
+            //brb
+        } else {
+            tooltip.addPara("This facility will be used to safely re-awaken people from cryosleep", Misc.getTooltipTitleAndLightHighlightColor(), 10f);
+
+        }
+    }
+
+    @Override
+    public void notifyBeingRemoved(MarketAPI.MarketInteractionMode mode, boolean forUpgrade) {
+        if(tiedEntity!=null){
+            tiedEntity.getMemoryWithoutUpdate().unset("$reawakening");
+
+        }
+        super.notifyBeingRemoved(mode, forUpgrade);
+    }
+
+    @Override
+    public String getBuildOrUpgradeProgressText() {
+        if (isUpgrading()) {
+            return "Re - awakening process " + this.getBuildOrUpgradeDaysText();
+            //return "" + (int) Math.round(Misc.getMarketSizeProgress(market) * 100f) + "%";
+
+        }
+        return super.getBuildOrUpgradeProgressText();
+    }
+
+    @Override
+    public boolean isAvailableToBuild() {
+        return false;
+    }
+
+    @Override
+    public boolean showWhenUnavailable() {
+        return false;
+    }
+
+    private void grow(int size) {
+        if (market.getSize() < size) {
+            market.removeCondition("population_" + market.getSize());
+            market.setSize((int) size);
+            market.addCondition("population_" + market.getSize());
+            market.getPopulation().setWeight(0.1f);
+            Misc.fadeAndExpire(tiedEntity, 1f);
+            tiedEntity = null;
+        }
+
+    }
+}
